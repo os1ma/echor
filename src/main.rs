68a3eefbb06c680efb35b01aadfe85cc7791dce4
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2022-09-16 15:00:28 +0900

    Parse arguments

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,36 @@
-use std::env;
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 4 {
-        print_usage();
+        eprint_usage();
         return;
     }
 
-    println!("Hello, world!");
+    let behavior_type = &args[1];
+    let ip_address = &args[2];
+    let port = &args[3];
+
+    match behavior_type.as_str() {
+        "server" => server(ip_address, port),
+        "client" => client(ip_address, port),
+        _ => {
+            eprintln!("Invalid argument. You must specify server or client.");
+            eprint_usage();
+            process::exit(1)
+        }
+    }
 }
 
-fn print_usage() {
-    println!("Usage: echo <server|client> <ip address> <port>")
+fn eprint_usage() {
+    eprintln!("Usage: echo <server|client> <ip address> <port>")
+}
+
+fn server(ip_address: &String, port: &String) {
+    println!("Server started {}:{}", ip_address, port);
+}
+
+fn client(ip_address: &String, port: &String) {
+    println!("Connecting to {}:{}", ip_address, port);
 }
