d446b5340ad588c4db206fed29304f3f216c4106
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2022-09-16 15:19:50 +0900

    Implement single thread server

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,25 @@
+use std::{
+    io::{BufRead, BufReader},
+    net::TcpListener,
+};
+
 pub fn run(ip_address: &String, port: &String) {
-    println!("Server started {}:{}", ip_address, port);
+    println!("Server starting {}:{}", ip_address, port);
+
+    let address = format!("{}:{}", ip_address, port);
+
+    let server = TcpListener::bind(address).unwrap();
+    server.set_nonblocking(true).unwrap();
+
+    loop {
+        if let Ok((client, addr)) = server.accept() {
+            println!("Connected by {}", addr);
+
+            let mut reader = BufReader::new(client);
+            let mut buf = String::new();
+            while let Ok(_) = reader.read_line(&mut buf) {
+                println!("{}", buf);
+            }
+        }
+    }
 }
