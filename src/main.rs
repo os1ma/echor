f7218ff6282fd46c2a606da7c0e19fd4f8411ddf
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2022-09-16 14:47:18 +0900

    Show usage if arguments length is invalid

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 4 {
+        print_usage();
+        return;
+    }
+
     println!("Hello, world!");
+}
+
+fn print_usage() {
+    println!("Usage: echo <server|client> <ip address> <port>")
 }
