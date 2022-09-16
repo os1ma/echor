046aa350a9a59cbe2de5cc6aff683c9ce67110b2
Yuki Oshima <39944763+os1ma@users.noreply.github.com>
2022-09-16 15:25:23 +0900

    Update server to use multi thread

--- src/server.rs
+++ src/server.rs
@@ -1,25 +1,31 @@
 use std::{
     io::{BufRead, BufReader},
-    net::TcpListener,
+    net::{TcpListener, TcpStream},
+    thread,
 };
 
 pub fn run(ip_address: &String, port: &String) {
-    println!("Server starting {}:{}", ip_address, port);
-
     let address = format!("{}:{}", ip_address, port);
-
     let server = TcpListener::bind(address).unwrap();
     server.set_nonblocking(true).unwrap();
+    println!("Server started {}:{}", ip_address, port);
 
     loop {
         if let Ok((client, addr)) = server.accept() {
             println!("Connected by {}", addr);
 
-            let mut reader = BufReader::new(client);
-            let mut buf = String::new();
-            while let Ok(_) = reader.read_line(&mut buf) {
-                println!("{}", buf);
-            }
+            thread::spawn(|| {
+                handle(client);
+            });
         }
+    }
+}
+
+fn handle(client: TcpStream) {
+    let mut reader = BufReader::new(client);
+    let mut buf = String::new();
+    while let Ok(_) = reader.read_line(&mut buf) {
+        print!("{}", buf);
+        buf = String::new()
     }
 }
